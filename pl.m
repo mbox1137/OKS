99e79c4c543f1967f11b41eb82e63ee53ec640f2
Egor <egrefim2020@mail.ru>
2024-03-18 11:17:35 +0300

    graphs

--- pl.m
+++ pl.m
@@ -1,4 +1,5 @@
 %mas=zeros(2,3);
-mas=[1,2,3;1,4,9];
-plot(mas(1,:),mas(2,:))
+mas=[1,2,3;1,4,9;2,6,4];
+plot(mas(1,:),mas(2,:),mas (1,:),mas(3,:))
+
 grid on;
--- prm.m
+++ prm.m
@@ -19,16 +19,18 @@ params.DT_ALL=abs(1);
 params.freqDownLink=7*10^9;  
 params.OKSType=0;
 params.min_elev_deg=10;
-tabl_Znach = zeros (2,3);
+tabl_Znach_RMS = zeros (4,12);
+tabl_Znach_Numposts = zeros (2,3);
+tabl_Znach_DistOP = zeros (2,3);
 
 tic
 k=1;
-for RMS_DOPPLER = 0.8:0.2:3; %цикл ра�?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
+for RMS_DOPPLER = 0.8:0.2:1; %цикл р��?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
     params.RMS_DOPPLER = RMS_DOPPLER;
     
 results=loshadka(params);
-tabl_Znach(1,k) = RMS_DOPPLER;
-tabl_Znach(2,k) = results.SrZnachOKS;
+tabl_Znach_RMS(1,k) = RMS_DOPPLER;
+tabl_Znach_RMS(2,k) = results.SrZnachOKS;
 disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
 disp(['RMS_DOPPLER = ' num2str(RMS_DOPPLER)  ' Гц ' ])
 k=k+1;
@@ -42,15 +44,15 @@ params.dY = dY;
 params.dZ = dZ;   
 
 
-for RMS_DOPPLER = 0.8:0.2:3; %цикл ра�?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
+for RMS_DOPPLER = 0.8:0.2:1; %цикл ра�?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
     params.RMS_DOPPLER = RMS_DOPPLER;
     
 results=loshadka(params);
-tabl_Znach(1,k) = RMS_DOPPLER;
-tabl_Znach(2,k) = results.SrZnachOKS;
+% tabl_Znach(1,k) = RMS_DOPPLER;
+tabl_Znach(3,k) = results.SrZnachOKS;
 disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
 disp(['RMS_DOPPLER = ' num2str(RMS_DOPPLER)  ' Гц ' ])
-k=k+1;
+% k=k+1;
 end
 
 dX = 2;   
@@ -61,139 +63,147 @@ params.dY = dY;
 params.dZ = dZ;  
 
 
-for RMS_DOPPLER = 0.8:0.2:3; %цикл ра�?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
+for RMS_DOPPLER = 0.8:0.2:1; %цикл ра�?�?чета OKS от SKO, при кол-во по�?тов 3шт, ра�?�?тро�?нии по�?тов от центра 350км
     params.RMS_DOPPLER = RMS_DOPPLER;
     
 results=loshadka(params);
 tabl_Znach(1,k) = RMS_DOPPLER;
-tabl_Znach(2,k) = results.SrZnachOKS;
+tabl_Znach(4,k) = results.SrZnachOKS;
 disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
 disp(['RMS_DOPPLER = ' num2str(RMS_DOPPLER)  ' Гц ' ])
 k=k+1;
 end
 
-dX = 0.5;  
-dY = 0.5;  
-dZ = -0.5;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;  
+% plot (tabl_Znach_RMS(1,:),tabl_Znach_RMS(2,:), tabl_Znach_RMS(3,:),tabl_Znach_RMS(4,:));
+X = tabl_Znach_RMS(1,:);
+Y1 = tabl_Znach_RMS(2,:);
+Y2 = tabl_Znach_RMS(3,:);
+Y3 = tabl_Znach_RMS(4,:);
 
-RMS_DOPPLER = 1;
- params.RMS_DOPPLER = RMS_DOPPLER;
-for NumPosts = 3:1:20; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
-   
-    params.NumPosts = NumPosts;
-    
-results=loshadka(params);
-tabl_Znach(1,k) = NumPosts;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
-disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
-k=k+1;
-end
+plot (X,Y1,X,Y2,X,Y3);
 
-dX = 1;  
-dY = 1;  
-dZ = -1;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;  
+% dX = 0.5;  
+% dY = 0.5;  
+% dZ = -0.5;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;  
 
+% RMS_DOPPLER = 1;
+%  params.RMS_DOPPLER = RMS_DOPPLER;
+% for NumPosts = 3:1:20; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
+%    
+%     params.NumPosts = NumPosts;
+%     
+% results=loshadka(params);
+% tabl_Znach(1,k) = NumPosts;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
+% disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
+% k=k+1;
+% end
+% 
+% dX = 1;  
+% dY = 1;  
+% dZ = -1;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;  
+% 
+% 
+% for NumPosts = 3:1:10; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
+%    
+%     params.NumPosts = NumPosts;
+%     
+% results=loshadka(params);
+% tabl_Znach(1,k) = NumPosts;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
+% disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
+% k=k+1;
+% end
 
-for NumPosts = 3:1:20; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
-   
-    params.NumPosts = NumPosts;
-    
-results=loshadka(params);
-tabl_Znach(1,k) = NumPosts;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
-disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
-k=k+1;
-end
+% dX = 2;  
+% dY = 2;  
+% dZ = -2;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;  
+% 
+% 
+% for NumPosts = 3:1:20; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
+%    
+%     params.NumPosts = NumPosts;
+%     
+% results=loshadka(params);
+% tabl_Znach(1,k) = NumPosts;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
+% disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
+% k=k+1;
+% end
+% 
+% dX = 0.5;  
+% dY = 0.5;  
+% dZ = -0.5;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;
+% 
+%  NumPosts = 3;
+%  params.NumPosts = NumPosts;
+% for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
+% 
+%    params.distOP = distOP;
+%    
+% results=loshadka(params);
+% tabl_Znach(1,k) = distOP;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
+% disp(['distOP = ' num2str(distOP)  ' км' ])
+% k=k+1;
+% end
 
-dX = 2;  
-dY = 2;  
-dZ = -2;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;  
-
-
-for NumPosts = 3:1:20; %цикл ра�?�?чета OKS от кол-ва по�?тов, SKO = 1гц, ра�?�?тро�?нии по�?тов от центра 350км
-   
-    params.NumPosts = NumPosts;
-    
-results=loshadka(params);
-tabl_Znach(1,k) = NumPosts;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) %
-disp(['NumPosts = ' num2str(NumPosts)  ' Штук' ])
-k=k+1;
-end
-
-dX = 0.5;  
-dY = 0.5;  
-dZ = -0.5;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;
-
- NumPosts = 3;
- params.NumPosts = NumPosts;
-for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
-
-   params.distOP = distOP;
-   
-results=loshadka(params);
-tabl_Znach(1,k) = distOP;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
-disp(['distOP = ' num2str(distOP)  ' км' ])
-k=k+1;
-end
-
-dX = 1;  
-dY = 1;  
-dZ = -1;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;
-
-
-for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
-
-   params.distOP = distOP;
-   
-results=loshadka(params);
-tabl_Znach(1,k) = distOP;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
-disp(['distOP = ' num2str(distOP)  ' км' ])
-k=k+1;
-end
-
-dX = 2;  
-dY = 2;  
-dZ = -2;  
-params.dX = dX;  
-params.dY = dY;  
-params.dZ = dZ;
-
-
-
-for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
-
-   params.distOP = distOP;
-   
-results=loshadka(params);
-tabl_Znach(1,k) = distOP;
-tabl_Znach(2,k) = results.SrZnachOKS;
-disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
-disp(['distOP = ' num2str(distOP)  ' км' ])
-k=k+1;
-end
+% dX = 1;  
+% dY = 1;  
+% dZ = -1;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;
+% 
+% 
+% for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
+% 
+%    params.distOP = distOP;
+%    
+% results=loshadka(params);
+% tabl_Znach(1,k) = distOP;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
+% disp(['distOP = ' num2str(distOP)  ' км' ])
+% k=k+1;
+% end
+% 
+% dX = 2;  
+% dY = 2;  
+% dZ = -2;  
+% params.dX = dX;  
+% params.dY = dY;  
+% params.dZ = dZ;
+% 
+% 
+% 
+% for distOP = 200:50:600;  %цикл ра�?чета OKS от ра�?�?тро�?нии по�?тов от центра, SKO = 1гц, кол-во по�?тов 3
+% 
+%    params.distOP = distOP;
+%    
+% results=loshadka(params);
+% tabl_Znach(1,k) = distOP;
+% tabl_Znach(2,k) = results.SrZnachOKS;
+% disp(['d OKS = ' num2str(results.SrZnachOKS)  ' км ' ]) 
+% disp(['distOP = ' num2str(distOP)  ' км' ])
+% k=k+1;
+% end
  disp (tabl_Znach)
 
 toc
